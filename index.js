296704749e27d4fdb955ac8ab7e8fbd45f35d36d
Zamzam <Zamzamhassan44@gmail.com>
2022-06-17 19:13:53 +0300

    algorithms palindrome

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  for(let i = 0; i<word.length /2 ; i++){
+    if(word[i] === word[word.length - 1 - i]){
+      return true
+    }
+      else{
+        return false
+      }
+    }
+  }
 
 /* 
   Add your pseudocode here
